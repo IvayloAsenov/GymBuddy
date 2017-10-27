b9e905c32d990488882911fadadf545dec9f1f07
IvayloAsenov <ivaylo.asenov@mail.mcgill.ca>
2017-10-26 23:58:46 -0400

    -Added image button to start workout in home screen and a timer appears that only counts up to 10 for now.

--- app/src/main/java/com/example/ivo/gymbuddy/Home.java
+++ app/src/main/java/com/example/ivo/gymbuddy/Home.java
@@ -8,7 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Home extends AppCompatActivity implements BodyTypes{
 
@@ -19,6 +26,13 @@ public class Home extends AppCompatActivity implements BodyTypes{
     Button b_viewWorkouts;
 
     ImageView body_type;
+    TextView tv_timer;
+
+    ImageButton ib_startWorkout;
+
+    int i = 0;
+
+    boolean b_workout = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +43,9 @@ public class Home extends AppCompatActivity implements BodyTypes{
         b_add_workout = (Button) findViewById(R.id.LogAWorkout);
         b_viewWorkouts = (Button) findViewById(R.id.b_viewWorkouts);
 
+        tv_timer = (TextView) findViewById(R.id.tv_timer);
         body_type = (ImageView) findViewById(R.id.body_type);
+        ib_startWorkout = (ImageButton) findViewById(R.id.ib_startWorkout);
 
         but.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +72,38 @@ public class Home extends AppCompatActivity implements BodyTypes{
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), SeeWorkouts.class);
                 startActivity(intent);
+            }
+        });
+
+        /*
+        Timer that runs the method every second and increments the timer
+        on click of StartWorkout
+         */
+
+        ib_startWorkout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Timer timer = new Timer();
+
+                b_workout = true;
+
+                tv_timer.setVisibility(View.VISIBLE);
+
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        Home.this.runOnUiThread(new Runnable() {
+                            public void run() {
+                                tv_timer.setText(Integer.toString(i));
+                                i++;
+
+                                if(i == 10)
+                                    timer.cancel();
+                            }
+                        });
+                    }
+                }, 1000, 1000);
+
             }
         });
     }
