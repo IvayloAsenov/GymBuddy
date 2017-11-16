09a7536541d43614362371a1117a422d9694d037
IvayloAsenov <ivaylo.asenov@mail.mcgill.ca>
2017-11-16 11:16:25 -0500

    - Updated Challenges class. Weekly challenges don't work yet

--- app/src/main/java/com/example/ivo/gymbuddy/Challenges.java
+++ app/src/main/java/com/example/ivo/gymbuddy/Challenges.java
@@ -35,28 +35,75 @@ public class Challenges {
         Calendar sCalendar = Calendar.getInstance();
         String today_day = sCalendar.getDisplayName(Calendar.DAY_OF_WEEK, Calendar.LONG, Locale.getDefault());
 
-        SharedPreferences sharedPref = activity.getApplicationContext().getSharedPreferences("saved_daily_date", Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = activity.getApplicationContext().getSharedPreferences("saved_challenges_dates", Context.MODE_PRIVATE);
         String stored_daily_day = sharedPref.getString("saved_daily_date", today_day);
+        String stored_weekly_day = sharedPref.getString("saved_weekly_date", today_day);
 
-        Toast.makeText(activity.getApplicationContext(), "" + today_day + "" + stored_daily_day, Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity.getApplicationContext(), "" + today_day + "" + stored_weekly_day, Toast.LENGTH_SHORT).show();
         verifyDailyDate(today_day, stored_daily_day);
+        verifyWeeklyDate(today_day, stored_weekly_day);
     }
 
     protected void verifyDailyDate(String today_day, String stored_daily_day){
         if(stored_daily_day.equals(today_day)) {
             // Keep challenge
-            saveDate(today_day);
+           // saveDate(today_day);
         }else{
             //Change daily challenge
             nv = (NavigationView) activity.findViewById(R.id.navigation);
             nv.getMenu().findItem(R.id.account).setTitle("CHANGE CHALLENGE!");
+            saveDate(today_day, 0);
         }
     }
 
-    protected void saveDate(String today_day){
-        SharedPreferences sharedPref = activity.getApplicationContext().getSharedPreferences("saved_daily_date", Context.MODE_PRIVATE);
+    protected void verifyWeeklyDate(String today_day, String stored_weekly_date){
+
+        int td = dayToNum(today_day);
+        int wd = dayToNum(stored_weekly_date);
+
+        int dif = wd - td;
+
+        Log.d("ALLALALA", " " + td + " " + wd);
+        if(dif == 1 || (dif == 6 && wd == 7)){
+            // Change daily challenge
+            nv = (NavigationView) activity.findViewById(R.id.navigation);
+            nv.getMenu().findItem(R.id.logout).setTitle("CHANGE WEEKLY CHALLENGE!");
+            saveDate(today_day, 1);
+        }else{
+            // Do nothing
+        }
+    }
+
+    private int dayToNum(String day){
+        if(day.equals("Monday"))
+            return 1;
+        else if(day.equals("Tuesday"))
+            return 2;
+        else if(day.equals("Wednesday"))
+            return 3;
+        else if(day.equals("Thursday"))
+            return 4;
+        else if(day.equals("Friday"))
+            return 5;
+        else if(day.equals("Saturday"))
+            return 6;
+        else if(day.equals("Sunday"))
+            return 7;
+
+        return 1;
+    }
+
+    protected void saveDate(String today_day, int w_d){
+        SharedPreferences sharedPref = activity.getApplicationContext().getSharedPreferences("saved_challenges_dates", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("saved_daily_date", today_day);
+
+        if(w_d == 0){
+            editor.putString("saved_daily_date", today_day);
+        }else{
+            editor.putString("saved_weekly_date", today_day);
+        }
         editor.commit();
     }
+
+
 }
