f10c3165d375e6e3c6131f7dd74dfabee376dc00
IvayloAsenov <ivaylo.asenov@hotmail.com>
2018-02-13 21:12:31 -0500

    Test push
    Todo
    - Comments
    - Fix bugs
    - Add some kind of economy to encourage progression
    - UI

--- app/src/main/java/com/example/ivo/gymbuddy/ChallengePackage/Challenges.java
+++ app/src/main/java/com/example/ivo/gymbuddy/ChallengePackage/Challenges.java
@@ -20,7 +20,7 @@ import java.util.Random;
 /**
  * Created by Ivo on 11/15/2017.
  * Added comments + weekly challenge 2 (problem with pushes!).
- * Test push 2
+ * Test push 3
  */
 
 public class Challenges implements DailyChallenges, WeeklyChallenges {
